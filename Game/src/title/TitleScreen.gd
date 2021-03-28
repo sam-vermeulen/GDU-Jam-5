714210e83514d997831ae60ede13295eb4b3a56e
DerekMarshall855 <derekmarshall855@gmail.com>
2021-03-28 17:26:37 -0400

    Fix title screen, add fade

--- Game/src/title/TitleScreen.gd
+++ Game/src/title/TitleScreen.gd
@@ -1,11 +1,17 @@
 extends Control
 
+var load_scene
+
 func _ready():
 	for button in $Menu/Center/Buttons.get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.target_scene])
 
 func _on_Button_pressed(target_scene):
+	load_scene = target_scene
 	if target_scene == "exit":
 		get_tree().quit()
-	else:
-		get_tree().change_scene(target_scene)
+	$FadeIn.show()
+	$FadeIn.fade_in()
+
+func _on_FadeIn_fade_finished():
+	get_tree().change_scene(load_scene)
